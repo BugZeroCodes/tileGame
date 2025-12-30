cea57596540de99754e0cdc32915760b812e727f
BugZeroCodes <tamadi293@gmail.com>
2025-12-30 13:12:06 -0500

    Add lava and game over animation

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ pyg.display.set_caption('Platformer')
 
 # define game variables
 tileSize = 50
+gameRun = 0
 
 # load images
 sunImage = pyg.image.load('img/sun.png')
@@ -47,6 +48,9 @@ class World(object):
                 if t == 3:
                     blob = Enemy(col*tileSize, rowCount*tileSize+15)
                     blobGroup.add(blob)
+                if t == 6:
+                    lava = Lava(col*tileSize, rowCount*tileSize+(tileSize//2))
+                    lavaGroup.add(lava)
                 col += 1
             rowCount += 1
 
@@ -66,6 +70,7 @@ class Player(object):
             img_left = pyg.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
             self.images_left.append(img_left)
+        self.deadImg = pyg.image.load('img/ghost.png')
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -76,76 +81,85 @@ class Player(object):
         self.jumped = False
         self.dir = 0
 
-    def update(self):
+    def update(self, gameRun):
         dx = 0
         dy = 0
         walkCooldown = 5
 
-        # get keypresses
-        key = pyg.key.get_pressed()
-        if key[pyg.K_SPACE] and self.jumped == False:
-            self.vel_y = -15
-            self.jumped = True
-        if not key[pyg.K_SPACE]:
-            self.jumped = False
-        if key[pyg.K_LEFT]:
-            dx -= 5
-            self.counter += 1
-            self.dir = -1
-        if key[pyg.K_RIGHT]:
-            dx += 5
-            self.counter += 1
-            self.dir = 1
-        if key[pyg.K_LEFT] == False  and key[pyg.K_RIGHT] == False:
-            self.counter = 0
-            self.index = 0
-            if self.dir == 1:
-                self.image = self.images_right[self.index]
-            else:
-                self.image = self.images_left[self.index]
-
-        # animation
-        if self.counter > walkCooldown:
-            self.counter = 0
-            self.index += 1
-            if self.index >= len(self.images_right):
+        if gameRun == 0:
+            # get keypresses
+            key = pyg.key.get_pressed()
+            if key[pyg.K_SPACE] and self.jumped == False:
+                self.vel_y = -15
+                self.jumped = True
+            if not key[pyg.K_SPACE]:
+                self.jumped = False
+            if key[pyg.K_LEFT]:
+                dx -= 5
+                self.counter += 1
+                self.dir = -1
+            if key[pyg.K_RIGHT]:
+                dx += 5
+                self.counter += 1
+                self.dir = 1
+            if key[pyg.K_LEFT] == False  and key[pyg.K_RIGHT] == False:
+                self.counter = 0
                 self.index = 0
-            if self.dir == 1:
-                self.image = self.images_right[self.index]
-            if self.dir == -1:
-                self.image = self.images_left[self.index]
+                if self.dir == 1:
+                    self.image = self.images_right[self.index]
+                else:
+                    self.image = self.images_left[self.index]
 
-        # add gravity
-        self.vel_y += 1
-        if self.vel_y > 10:
-            self.vel_y = 10
-        dy += self.vel_y
+            # animation
+            if self.counter > walkCooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+                if self.dir == 1:
+                    self.image = self.images_right[self.index]
+                if self.dir == -1:
+                    self.image = self.images_left[self.index]
 
-        # check collisions
-        for t in world.tiles:
-            #collision in x direction
-            if t[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                dx = 0
-            # collision in y direction
-            if t[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                # below the ground?
-                if self.vel_y < 0:
-                    dy = t[1].bottom - self.rect.top
-                    self.vel_y = 0
-                elif self.vel_y >= 0:
-                    dy = t[1].top - self.rect.bottom
-                    self.vel_y = 0
+            # add gravity
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
 
-        # update coordinates
-        self.rect.x += dx
-        self.rect.y += dy
+            # check collisions
+            for t in world.tiles:
+                #collision in x direction
+                if t[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                # collision in y direction
+                if t[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    # below the ground?
+                    if self.vel_y < 0:
+                        dy = t[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    elif self.vel_y >= 0:
+                        dy = t[1].top - self.rect.bottom
+                        self.vel_y = 0
 
-        if self.rect.bottom > height:
-            self.rect.bottom = height
-            dy = 0
+            # check enemy collisions
+            if pyg.sprite.spritecollide(self, blobGroup, False):
+                gameRun = -1
+            if pyg.sprite.spritecollide(self, lavaGroup, False):
+                gameRun = -1
+
+            # update coordinates
+            self.rect.x += dx
+            self.rect.y += dy
+        elif gameRun == -1:
+            self.image = self.deadImg
+            if self.rect.y > 200:
+                self.rect.y -= 5
 
         # draw player on screen
         src.blit(self.image, self.rect)
+
+        return gameRun
 
 class Enemy(pyg.sprite.Sprite):
     def __init__(self, x, y):
@@ -163,6 +177,15 @@ class Enemy(pyg.sprite.Sprite):
         if self.moveCounter > 50:
             self.moveDir *= -1
             self.moveCounter *= -1
+
+class Lava(pyg.sprite.Sprite):
+    def __init__(self, x, y):
+        pyg.sprite.Sprite.__init__(self)
+        img = pyg.image.load('img/lava.png')
+        self.image = pyg.transform.scale(img, (tileSize, tileSize // 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 worldData = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -187,7 +210,9 @@ worldData = [
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+lavaGroup = pyg.sprite.Group()
 blobGroup = pyg.sprite.Group()
+
 world = World(worldData)
 plr = Player(100, height - 130)
 
@@ -200,10 +225,14 @@ while run:
     src.blit(sunImage, (100, 100))
 
     world.draw()
-    plr.update()
 
-    blobGroup.update()
+    if gameRun == 0:
+        blobGroup.update()
+
+    gameRun = plr.update(gameRun)
+
     blobGroup.draw(src)
+    lavaGroup.draw(src)
 
     for e in pyg.event.get():
         if e.type == pyg.QUIT:
